89a4afa27212e3ca49d57916afdb197599e94e34
UPRCHKA <xxxenodochialritchie@gmail.com>
2022-05-25 22:16:18 +0300

    add alembic migrations

---
+++ bibliotheca/app.py
@@ -0,0 +1,206 @@
+from fastapi import Depends, FastAPI, Body, HTTPException, Cookie
+from fastapi.responses import Response, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from typing import Optional
+from pydantic import Json
+from crud import SALT
+import crud #, models
+import schemas
+import json
+import re
+import hmac
+import hashlib
+import base64
+
+
+MAIN_PAGE_NON_LOGIN = './static/templates/main_page_non_login.html'
+MAIN_PAGE = './static/templates/main_page.html'
+LOGIN = './static/templates/login.html'
+REGISTRATION_PAGE = './static/templates/registration_page.html'
+SECRET_KEY = '6dfd98adf2601c1f54c794fb8376794805972d71d906a3021525b90e54911a4f'
+
+#models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/add", response_model=schemas.BookOut)
+def add_book(book: schemas.BookAdd = Body(...), db: Session = Depends(get_db)):
+    db_book = crud.add_book(db=db, book=book)
+    if not db_book:
+        raise HTTPException(status_code=400, detail="Book already exists")
+    return db_book
+
+
+@app.get("/search/")
+def search_book_author(search_request: str, db: Session = Depends(get_db)):
+    db_book = crud.get_books_by_title(db=db, title=search_request)
+    if db_book:
+        book_list = {"books":[schemas.BookOut.from_orm(book).dict() for book in db_book]} 
+        return Response(json.dumps(book_list), media_type='application/json')
+    authors_list = [author.strip() for author in re.split(r'[;,]',search_request)]
+    all_books_by_author = crud.get_all_authors_books(db=db, list_of_authors_fullnames=authors_list)
+    if all_books_by_author:
+        book_list = {"books":[schemas.BookOut.from_orm(book).dict() for book in all_books_by_author]}
+        return Response(json.dumps(book_list), media_type='application/json')
+    raise HTTPException(status_code=400, detail="Nothing was found for your query")
+    
+
+@app.put("/edit")
+def edit_exists_book(book: schemas.BookChange, db: Session = Depends(get_db)):
+    db_book = crud.book_change(book=book, db=db)
+    if db_book:
+        return db_book
+    raise HTTPException(status_code=400, detail="Apparently such a book already exists(")
+
+
+@app.delete("/book/{id}")
+def delete_book(id: int, db: Session = Depends(get_db)):
+    if crud.delete_book_by_id(db=db, id=id):
+        return Response(json.dumps({"details": 'This book is deleted!'}), media_type='application/json')
+    raise HTTPException(status_code=400, detail="Something wrong!")
+
+
+class CheckSession():
+    def __init__(self) -> None:
+        self.SESSION_IS_NOT = None
+        self.SESSION_IS_INVALID = None
+        self.bad_response = None
+    
+    def check(self, session: Optional[str]) -> None:
+        if not session:
+            self.SESSION_IS_NOT = True
+            return
+        if self.get_permission(session):
+            self.SESSION_IS_INVALID = False
+            self.SESSION_IS_NOT = False
+        else:
+            self.SESSION_IS_INVALID = True
+
+    def set_bad_response(self, response):
+        self.bad_response = response
+
+    #return correct email of username or None if sign is not True
+    @staticmethod
+    def get_permission(session):
+        username_b64, sign = session.split(".")
+        username = base64.b64decode(username_b64).decode()
+        if hmac.compare_digest(sign_data(username), sign):
+            return username
+
+
+#Creates hmac for username with secret key
+def sign_data(data: str) -> str:
+    return hmac.new(
+        SECRET_KEY.encode(),
+        msg=data.encode(),
+        digestmod=hashlib.sha256
+    ).hexdigest().upper()
+
+
+#Check the user's password hash in the database with the password from the web form
+def check_password(db: Session, users_email: str, password: str):
+    user = crud.get_user_by_email(db=db, users_email=users_email)
+    users_hash_password = user.hashed_password
+    verifiable_hash_password = hashlib.sha256((SALT + password).encode()).hexdigest()
+    return users_hash_password == verifiable_hash_password
+    
+
+@app.get('/')
+def index_page(session: Optional[str] = Cookie(default=None)):
+    with open(MAIN_PAGE_NON_LOGIN, 'r') as html_file:
+        non_login_page = html_file.read()
+
+    check_session = CheckSession()
+    check_session.check(session)
+
+    if check_session.SESSION_IS_NOT:
+        return Response(non_login_page, media_type='text/html')
+
+    if check_session.SESSION_IS_INVALID:
+        response = Response(non_login_page, media_type='text/html')
+        response.delete_cookie(key="session")
+        return response
+    with open(MAIN_PAGE, 'r') as html_file:
+        main_page = html_file.read()
+    return Response(main_page, media_type='text/html')
+
+
+@app.get('/login')
+def login(session: Optional[str] = Cookie(default=None)):
+    with open(LOGIN, 'r') as html_file:
+        login_page = html_file.read()
+
+    check_session = CheckSession()
+    check_session.check(session)
+
+    if check_session.SESSION_IS_NOT:
+        return Response(login_page, media_type='text/html')
+
+    if check_session.SESSION_IS_INVALID:
+        response = Response(login_page, media_type='text/html')
+        response.delete_cookie(key="session")
+        return response
+    with open(MAIN_PAGE, 'r') as html_file:
+        main_page = html_file.read()
+    return Response(main_page, media_type='text/html')
+
+
+@app.post('/login')
+def process_login_page(user: schemas.UserIn = Body(...), db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db=db, users_email=user.email)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Dont auth")
+    elif check_password(db=db, users_email=user.email, password=user.password):
+        key_for_cookie = 'session'
+        signed_username = base64.b64encode(db_user.email.encode()).decode() + '.' + sign_data(db_user.email)
+        response = Response(json.dumps({"username": f'{db_user.name}'}), media_type='application/json')
+        response.set_cookie(key=key_for_cookie, value=signed_username)
+        return response
+    raise HTTPException(status_code=400, detail="Dont auth")
+
+
+@app.get('/logout')
+def logout():
+    response = Response()
+    response.delete_cookie(key="session")
+    return response
+
+
+@app.get('/registration')
+def get_registration(session: Optional[str] = Cookie(default=None)):
+    check_session = CheckSession()
+    check_session.check(session)
+    if check_session.SESSION_IS_NOT:
+        with open(REGISTRATION_PAGE, 'r') as regiistation_html:
+            registration_page = regiistation_html.read()
+        return Response(registration_page, media_type='text/html')
+    if check_session.SESSION_IS_INVALID:
+        response = Response(registration_page, media_type='text/html')
+        response.delete_cookie(key="session")
+        return response
+    return RedirectResponse(url='/', status_code=303)
+
+
+@app.post('/registration')
+def post_registration(user: schemas.UserIn = Body(...), db: Session = Depends(get_db)): 
+    db_user = crud.get_user_by_email(db=db, users_email=user.email)
+    if not db_user:
+        db_user = crud.create_user(db=db, user=user)
+        key_for_cookie = 'session'
+        signed_username = base64.b64encode(user.email.encode()).decode() + '.' + sign_data(user.email)
+        response = Response(json.dumps({"name": f'{user.name}'}), media_type='application/json')
+        response.set_cookie(key=key_for_cookie, value=signed_username)
+        return response
+    else:
+        raise HTTPException(status_code=400, detail="The user already exists")
---
+++ bibliotheca/crud.py
@@ -0,0 +1,107 @@
+from statistics import mode
+from sqlalchemy.orm import Session
+from sqlalchemy import or_, and_, delete
+from database import engine
+import hashlib
+import models
+import schemas
+
+SALT = 'b8c4703bffd39be0729fe85bf4d798bd4d5809f8121a81f8cfb20a286fec6104'
+
+def get_user_by_email(db: Session, email: str):
+    pass
+
+def get_all_authors_books(db: Session, list_of_authors_fullnames: list):
+    expressions = [models.Author.full_name==full_name for full_name in list_of_authors_fullnames]
+    queries_for_exists = [db.query(models.Author)
+                        .join(models.authors_book_table)
+                        .filter(expression, models.Book.id==models.authors_book_table.c.book_id).exists() 
+                        for expression in expressions]
+    last_expression = db.query(models.Author) \
+                        .join(models.authors_book_table) \
+                        .filter(and_(~models.Author.full_name.in_(list_of_authors_fullnames), models.Book.id==models.authors_book_table.c.book_id))
+
+    all_books_by_authors = db.query(models.Book).filter(*queries_for_exists, ~last_expression.exists()).all()
+    return all_books_by_authors
+
+def add_book(db: Session, book: schemas.BookAdd):
+    authors = [author.full_name for author in book.authors]
+    expressions = [models.Author.full_name==author for author in authors]
+    db_authors = db.query(models.Author).filter(or_(*expressions)).all()
+
+    if len(authors) != len(db_authors):
+        db_book = models.Book(title=book.title, description=book.description)
+        for author in authors:
+            db_author = get_author_by_fullname(db=db, full_name=author)
+            if not db_author:
+                db_author = models.Author(full_name=author)
+                db.add(db_author)
+            db_book.authors.append(db_author)
+        db.add(db_book)
+        db.commit()
+        db.refresh(db_book)
+        return db_book
+    all_books_by_authors = get_all_authors_books(db=db, list_of_authors_fullnames=authors)
+    for db_book in all_books_by_authors:
+        if db_book.title == book.title:
+            return False
+
+    db_book = models.Book(title=book.title, description=book.description)
+    db_book.authors.extend(db_authors)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+def get_books_by_title(db: Session, title: str):
+    return db.query(models.Book).filter(models.Book.title == title).all()
+
+def get_author_by_fullname(db: Session, full_name: str):
+    return db.query(models.Author).filter(models.Author.full_name == full_name).first()
+
+
+def book_change(db: Session, book: schemas.BookChange):
+    #id=5 title='book2' authors=[Author(id=None, full_name='author1', fast_facts=None), Author(id=None, full_name='author2', fast_facts=None)]
+    authors = [author.full_name for author in book.authors]
+    all_auth_books = get_all_authors_books(db=db, list_of_authors_fullnames=authors)
+    if all_auth_books:
+        for auth_book in all_auth_books:
+            if book.title == auth_book.title:
+                return False
+    db_book = db.query(models.Book).filter(models.Book.id == book.id).first()
+    db_book.authors = []
+    for author in authors:
+        db_author = get_author_by_fullname(db=db, full_name=author)
+        if not db_author:
+            db_author = models.Author(full_name=author)
+            db.add(db_author)
+        db_book.authors.append(db_author)
+    db_book.title = book.title
+    db_book.description = book.description
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+def delete_book_by_id(db: Session, id: int):
+    db_book = db.query(models.Book).get(id)
+    if db_book:
+        db_book.authors = []
+        db.commit()
+        engine.execute(delete(models.Book).where(models.Book.id==id))
+        return True
+    return False
+
+
+def get_user_by_email(db: Session, users_email: str):
+    return db.query(models.User).filter(models.User.email==users_email).first()
+
+def create_user(db: Session, user: schemas.UserIn):
+    hashed_password = hashlib.sha256((SALT + user.password).encode()).hexdigest()
+    db_user = models.User(email=user.email, name=user.name, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
